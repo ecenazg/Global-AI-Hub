70d2ae1b118c096f51006cc700fd4161acffaaba
Ecenaz Güngör <81537174+ecenazg@users.noreply.github.com>
2023-03-09 00:37:49 +0300

    update

--- pizza.py
+++ pizza.py
@@ -1,20 +1,20 @@
 import csv
 import datetime
 
-with open("PizzaMenu.txt", "w") as menu:
-    menu.write("""* Please Choose a Pizza Base:
-1: Classic
-2: Margherita
-3: FarmersPizza
-4: PlainPizza
+with open("Menu.txt", "w") as menu:
+    menu.write("""*Lütfen Bir Pizza Tabanı Seçiniz:
+1: Klasik
+2: Margarita
+3: TürkPizza
+4: SadePizza
 * and toppings of your choice:
-11: Olives
-12: Mushrooms
-13: GoatCheese
-14: Tuna
-15: Onions
-16: Corn
-* Thank you!\n""")
+11: Zeytin
+12: Mantarlar
+13: KeçiPeyniri
+14: Et
+15: Soğan
+16: Mısır
+* Teşekkür Ederiz!\n""")
 
 
 # makes a dict out of the menu options:
@@ -36,29 +36,29 @@ class Pizza:
         return self.__class__.cost
 
 
-class Margherita(Pizza):
+class Margarita(Pizza):
     cost = 35.0
 
     def __init__(self):
         self.description = "tomato sauce, Mozzarella and Basil"
 
 
-class Classic(Pizza):
+class Klasik(Pizza):
     cost = 34.0
 
     def __init__(self):
         self.description = "tomato sauce, cheese and olives/mushrooms"
 
 
-class FarmersPizza(Pizza):
+class TürkPizza(Pizza):
     cost = 36.0
 
     def __init__(self):
         self.description = "tomato sauce, extra cheese, eggplant, bell pepper and jalapeno"
 
 
-class PlainPizza(Pizza):
-    cost = 34
+class SadePizza(Pizza):
+    cost = 30.0
 
     def __init__(self):
         self.description = "tomato sauce, cheese"
@@ -77,42 +77,42 @@ class Decorator(Pizza):
           ' ' + Pizza.get_description(self)
 
 
-class Olives(Decorator):
+class Zeytin(Decorator):
     cost = 0.25
 
     def __init__(self, topping):
         Decorator.__init__(self, topping)
 
 
-class Mushrooms(Decorator):
-    cost = 0.25
-
-    def __init__(self, topping):
-        Decorator.__init__(self, topping)
-
-
-class GoatCheese(Decorator):
-    cost = 0.75
-
-    def __init__(self, topping):
-        Decorator.__init__(self, topping)
-
-
-class Tuna(Decorator):
+class Mantarlar(Decorator):
     cost = 0.5
 
     def __init__(self, topping):
         Decorator.__init__(self, topping)
 
 
-class Onions(Decorator):
+class KeçiPeyniri(Decorator):
+    cost = 0.75
+
+    def __init__(self, topping):
+        Decorator.__init__(self, topping)
+
+
+class Et(Decorator):
+    cost = 0.5
+
+    def __init__(self, topping):
+        Decorator.__init__(self, topping)
+
+
+class Soğan(Decorator):
     cost = 0.25
 
     def __init__(self, topping):
         Decorator.__init__(self, topping)
 
 
-class Corn(Decorator):
+class Mısır(Decorator):
     cost = 0.25
 
     def __init__(self, topping):
@@ -124,7 +124,7 @@ def main():
         for l in cust_menu:
             print(l, end="")
 
-    class_dict = {1: Classic, 2: Margherita, 3: FarmersPizza, 4: PlainPizza, 11: Olives, 12: Mushrooms, 13: GoatCheese, 14: Tuna, 15: Onions, 16: Corn}
+    class_dict = {1: Klasik, 2: Margarita, 3: TürkPizza, 4: SadePizza, 11: Zeytin, 12: Mantarlar, 13: KeçiPeyniri, 14: Et, 15: Soğan, 16: Mısır}
 
     code = input("Please choose the number of a basic pizza: ")
     while code not in ["1", "2", "3", "4"]:
